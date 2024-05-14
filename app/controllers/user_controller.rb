6ddd61de57ee8d855880cf6b339d0e1393007454
hal39@Desktop <hal39making@gmail.com>
2024-05-15 01:46:43 +0900

    homework 20240518

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,22 @@
 class UserController < ApplicationController
   def index
     @users = [
-      "Tanaka",
-      "Takahashi",
-      "Yamada",
-      "Suzuki"
+      {
+        name: "Tanaka",
+        age: 11
+      },
+      {
+        name: "Takahashi",
+        age: 22
+      },
+      {
+        name: "Yamada",
+        age: 33
+      },
+      {
+        name: "Suzuki",
+        age: 44
+      }
     ]
   end
 end
