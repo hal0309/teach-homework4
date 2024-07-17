ea648d95ba463c89a91bb90ff3096d3fef2f0a28
hal39@SurfaceLaptop5 <hal39making@gmail.com>
2024-07-17 16:31:50 +0900

    homework 20240721 ans

--- _withoutRuby/js/Calc.js
+++ _withoutRuby/js/Calc.js
@@ -1,57 +1,31 @@
 // 関数を定義
-function add() {
+function calc(method) {
     const num1 = parseFloat(document.getElementById("num1").value);
     const num2 = parseFloat(document.getElementById("num2").value);
     let result;
 
     if (isNaN(num1) || isNaN(num2)) {
         result = "数値を入力してください";
-    }else {
-        result = num1 + num2;
+    }else if (method === "add") {
+        result = `${num1} + ${num2} = <span class="bold">${num1 + num2}</span>`
+    }else if (method === "sub") {
+        result = `${num1} - ${num2} = <span class="bold">${num1 - num2}</span>`
+    }else if (method === "mul") {
+        result = `${num1} × ${num2} = <span class="bold">${num1 * num2}</span>`
+    }else if (method === "div") {
+        if (num2 === 0) {
+            result = "0で割ることはできません";
+        }else {
+            result = `${num1} ÷ ${num2} = <span class="bold">${num1 / num2}</span>`
+        }
     }
-    document.getElementById("result").textContent = result;
-}
 
-function sub() {
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
-    let result;
 
-    if (isNaN(num1) || isNaN(num2)) {
-        result = "数値を入力してください";
-    }else {
-        result = num1 - num2;
-    }
-    document.getElementById("result").textContent = result;
-}
-
-function mul() {
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
-    let result;
-
-    if (isNaN(num1) || isNaN(num2)) {
-        result = "数値を入力してください";
-    }else {
-        result = num1 * num2;
-    }
-    document.getElementById("result").textContent = result;
-}
-
-function div() {
-    const num1 = parseFloat(document.getElementById("num1").value);
-    const num2 = parseFloat(document.getElementById("num2").value);
-    let result;
-
-    if (isNaN(num1) || isNaN(num2)) {
-        result = "数値を入力してください";
-    }else if (num2 === 0) {
-        result = "0で割ることはできません";
-    }
-    else {
-        result = num1 / num2;
-    }
-    document.getElementById("result").textContent = result;
+    // resultをp要素で囲み、idがresultの要素の最上部に追加
+    const resultElement = document.getElementById("result");
+    const p = document.createElement("p");
+    p.innerHTML = result;
+    resultElement.insertAdjacentElement("afterbegin", p);
 }
 
 // ボタン要素を取得
@@ -61,7 +35,7 @@ const multiplyBtn = document.getElementById("multiplyBtn");
 const divideBtn = document.getElementById("divideBtn");
 
 // クリックイベントリスナーを設定
-plusBtn.addEventListener("click", add);
-minusBtn.addEventListener("click", sub);
-multiplyBtn.addEventListener("click", mul);
-divideBtn.addEventListener("click", div);
+plusBtn.addEventListener("click", () => calc("add"));
+minusBtn.addEventListener("click", () => calc("sub"));
+multiplyBtn.addEventListener("click", () => calc("mul"));
+divideBtn.addEventListener("click", () => calc("div"));
